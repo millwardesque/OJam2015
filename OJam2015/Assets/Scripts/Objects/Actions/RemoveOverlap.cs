e1a573547abe6db32db2bef4d7ea3541e8e05de8
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-27 13:05:45 -0400

    Adding destroy overlap script
    Expects asset placements styles assets

---
+++ OJam2015/Assets/Scripts/Objects/Actions/RemoveOverlap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RemoveOverlap : MonoBehaviour {
+	public void OnGui() {
+		DestroyOverlappedAssets ();
+	}
+
+	void DestroyOverlappedAssets () {
+		var otherAssets = Physics2D.OverlapCircleAll (new Vector2 (this.gameObject.transform.localPosition.x, 
+		                                                           this.gameObject.transform.localPosition.y),
+		                                              0.1f);
+		var distance = Mathf.Infinity;
+		foreach (var asset in otherAssets) {
+			if (asset.transform.parent != null && asset.transform.parent.name == this.gameObject.transform.parent.name) {
+				if (asset.gameObject != this.gameObject) {
+					DestroyImmediate (asset.gameObject);
+				}
+			}
+		}
+	}
+}
