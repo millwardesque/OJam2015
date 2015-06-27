cc8c85ddc39ee8842ee4021a0f605fcf8ed08303
Christopher Millward <christopher.millward@gmail.com>
2015-06-26 20:08:11 -0400

    Added player / flame prefabs, scripts, and stub textures.

---
+++ OJam2015/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody2D rigidbody;
+	public float speed = 1f;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		Vector2 force = new Vector2(x, y) * speed;
+
+		this.rigidbody.AddForce(force);
+	}
+}
