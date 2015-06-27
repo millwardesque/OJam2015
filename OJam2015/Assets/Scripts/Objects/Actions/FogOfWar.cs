59502da5422cbc987748bacf01ea167986d7745e
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-27 16:47:41 -0400

    Removed Test Code

---
+++ OJam2015/Assets/Scripts/Objects/Actions/FogOfWar.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWar : MonoBehaviour {
+	
+	SpriteRenderer renderer = null;
+	
+	bool isRevealed = false;
+	
+	void Start () {
+
+		renderer = this.gameObject.transform.parent.GetComponent<SpriteRenderer> ();
+		
+		renderer.color = Color.black;
+		
+	}
+	
+	// Update is called once per frame
+	
+	float updateTime = 0.1f;
+	
+	void Update () {
+
+		//TODO Can't get this to work
+		return;
+
+
+
+
+		if (isRevealed) {
+			return;
+		}
+		
+		updateTime -= Time.deltaTime;
+		
+		if (updateTime > 0) {
+			return;
+		}
+		
+		updateTime = 0.1f;
+		
+		
+		Vector2 direction = Vector2.one;
+		
+		if (Player.playerOne != null) {
+			direction =  Player.playerOne.transform.position - gameObject.transform.position;
+			
+			if(Mathf.Sqrt(direction.x * direction.x + direction.y * direction.y) > 8) {
+
+
+				return;
+			}
+
+			//renderer.color = Color.gray;
+			
+			direction = direction.normalized;
+			
+			RaycastHit2D hit = Physics2D.Raycast (gameObject.transform.position, direction, 8);
+			
+			if (hit.collider != null) {
+				if(hit.collider.tag == "PlayerVision") {
+					renderer.color = Color.white;
+					isRevealed = true;
+				} else {
+					Debug.Log(hit.collider.tag);
+				}
+			}
+			
+		}
+	}
+}
---
+++ OJam2015/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	public static Player playerOne = null;
+
+	public enum PlayerType {
+		One,
+		Two
+	}
+
+	public PlayerType type = PlayerType.One;
+
+	void Start () {
+		if (type == PlayerType.One) {
+			playerOne = this;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
