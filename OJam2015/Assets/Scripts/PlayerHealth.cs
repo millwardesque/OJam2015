ef9d80a46873169addc973999b04ba29b668f18b
Christopher Millward <christopher.millward@gmail.com>
2015-06-27 18:50:23 -0400

    removed debug calls from playerhealth.

--- OJam2015/Assets/Scripts/PlayerHealth.cs
+++ OJam2015/Assets/Scripts/PlayerHealth.cs
@@ -4,12 +4,10 @@ using System.Collections;
 public class PlayerHealth : Health {
 
 	protected override void OnChange(int oldHP, int newHP) {
-		base.OnChange (oldHP, newHP);
 		GUIManager.Instance.UpdateGUI();
 	}
 
 	protected override void OnDead() {
-		base.OnDead();
 		GameManager.Instance.OnPlayerDead();
 	}
 }
