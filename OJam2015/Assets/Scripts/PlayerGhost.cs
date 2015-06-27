f8e236950d04c94c67a88c888bc1967998da0494
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-27 14:27:38 -0400

    Adding player ghost

---
+++ OJam2015/Assets/Scripts/PlayerGhost.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGhost : MonoBehaviour {
+	public GameObject yourWorldOrgin = null;
+
+	public Vector3 YourWorldOrgin {
+		get {
+			if(yourWorldOrgin) {
+				return yourWorldOrgin.transform.position;
+			}
+
+			return Vector3.zero;
+		}
+	}
+
+	public GameObject theirWorldOrgin = null;
+
+	public Vector3 TheirWorldOrgin {
+		get {
+			if(theirWorldOrgin) {
+				return theirWorldOrgin.transform.position;
+			}
+			
+			return Vector3.zero;
+		}
+	}
+
+	public GameObject ownedPlayer = null;
+
+	public Vector3 OwnedPlayer {
+		get {
+			if(ownedPlayer) {
+				return ownedPlayer.transform.position;
+			}
+			
+			return Vector3.zero;
+		}
+	}
+
+	void Update () {
+		float x = -YourWorldOrgin.x + TheirWorldOrgin.x + OwnedPlayer.x;
+		float y = -YourWorldOrgin.y + TheirWorldOrgin.y + OwnedPlayer.y;
+		float z = OwnedPlayer.z;
+
+		gameObject.transform.position = new Vector3 (x, y, z);
+	}
+}
