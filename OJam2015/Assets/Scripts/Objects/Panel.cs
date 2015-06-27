5c32e25f86bfe45c071708fedf4e10aa17a5a48c
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-26 21:32:21 -0400

    Panel opens door

---
+++ OJam2015/Assets/Scripts/Objects/Panel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Panel : MonoBehaviour {
+	
+	public enum TriggerType {
+		Destroy
+	}
+	
+	public TriggerType type = TriggerType.Destroy;
+	
+	public List<GameObject> triggerObjects = new List<GameObject>();
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider2D) {
+		if (type == TriggerType.Destroy) {
+			if (collider2D.gameObject.tag == "Player") {
+				foreach(GameObject triggerObject in triggerObjects) {
+					GameObject.Destroy(triggerObject);
+				}
+				
+				triggerObjects.Clear();
+			}
+		}
+	}
+}
