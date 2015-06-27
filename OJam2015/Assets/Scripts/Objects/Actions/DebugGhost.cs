63632a88bcf0694578a489c041c9d5a6a34d03dd
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-27 09:50:36 -0400

    Added debug script for 'ghosting' tiles
    For multi level editing

---
+++ OJam2015/Assets/Scripts/Objects/Actions/DebugGhost.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Action for debug ghosting tiles. For editing layered levels
+
+public class DebugGhost : MonoBehaviour {
+	public void GhostOut() {
+		if (gameObject.GetComponent<SpriteRenderer> ())
+			gameObject.GetComponent<SpriteRenderer> ().color = new Color (gameObject.GetComponent<SpriteRenderer> ().color.r, 
+			                                                              gameObject.GetComponent<SpriteRenderer> ().color.g, 
+			                                                              gameObject.GetComponent<SpriteRenderer> ().color.b, 
+			                                                              0.2f); 
+		if(gameObject.GetComponent<Collider2D> ())
+			gameObject.GetComponent<Collider2D> ().enabled = false;
+		
+	}
+}
---
+++ OJam2015/Assets/Scripts/Objects/Actions/DebugGhostChildren.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Action for ghosting tiles. Meant for sake of testing, and not gameplay
+
+public class DebugGhostChildren : MonoBehaviour {	
+	public void Start() {
+		gameObject.BroadcastMessage ("GhostOut");
+	}
+}
