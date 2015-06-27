175f987c2b8a54111cf01c1ebdd20f27819f32f8
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-26 22:31:07 -0400

    Added repeating level
    Meant to be one half of a simple dual screen level.
    Use level 2 for testing / additions

---
+++ OJam2015/Assets/Scripts/Objects/End.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class End : MonoBehaviour {
+	public GameObject endPoint;
+	
+	void OnTriggerEnter2D(Collider2D collider2D) {
+		if(collider2D.tag == "Player" && endPoint) {
+			collider2D.gameObject.transform.position = endPoint.transform.position;
+		}
+	}
+}
