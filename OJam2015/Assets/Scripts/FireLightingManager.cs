245ef057d7f7a76dca890789ca5487a7d66b955b
Christopher Millward <christopher.millward@gmail.com>
2015-06-28 09:54:27 -0400

    Added ambient lighting pulsing to go with fire.

--- OJam2015/Assets/Scripts/FireLightingManager.cs
+++ OJam2015/Assets/Scripts/FireLightingManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class FireLightingManager : MonoBehaviour {
+	public float cycleDuration = 2f;
+	float cycleTimeRemaining = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +12,12 @@ public class FireLightingManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		cycleTimeRemaining -= Time.deltaTime;
+		while (cycleTimeRemaining < 0f) {
+			cycleTimeRemaining += cycleDuration;
+		}
+
+		bool direction = (cycleTimeRemaining / cycleDuration) > 0.5f;
+		RenderSettings.ambientIntensity = Mathf.Lerp(0.4f, 1f, (direction ? cycleTimeRemaining / cycleDuration : 1f - (cycleTimeRemaining / cycleDuration)));
 	}
 }
