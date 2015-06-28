3bcdb24e3308512d0f208214cd14ba744618700b
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-06-28 11:15:33 -0400

    Added simple score, and go to win state

--- OJam2015/Assets/Scripts/Objects/End.cs
+++ OJam2015/Assets/Scripts/Objects/End.cs
@@ -5,8 +5,9 @@ public class End : MonoBehaviour {
 	public GameObject endPoint;
 	
 	void OnTriggerEnter2D(Collider2D collider2D) {
-		if(collider2D.tag == "Player" && endPoint) {
-			collider2D.gameObject.transform.position = endPoint.transform.position;
+		if(collider2D.tag == "Player") {
+
+			GameManager.Instance.OnWin();
 		}
 	}
 }
