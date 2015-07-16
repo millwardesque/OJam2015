09df571be270ed1138d33d83a2d77d563f233ce8
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-07-16 17:08:09 -0400

    Tossing on untested mobile controls

---
+++ OJam2015/Assets/MobileControls.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileControls : MonoBehaviour 
+{
+	void Update () 
+	{
+		transform.Translate(Input.acceleration.x, -Input.acceleration.z, 0);
+	}
+}
